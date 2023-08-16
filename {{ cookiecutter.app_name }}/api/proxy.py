ca4ce7de7c8127321b2d189a99c3e4354e95be63
Lorenzo Bugli <lorenzo@fiscozen.it>
2023-08-16 16:59:12 +0200

    refactor(empty line): added empty line at end of file

--- {{ cookiecutter.app_name }}/api/proxy.py
+++ {{ cookiecutter.app_name }}/api/proxy.py
@@ -4,4 +4,4 @@ from __future__ import annotations
 
 # To ensure future compatibility, the methods of the Proxy module must return native Python data structures
 # (Lists, Dictionaries, etc...) avoiding categorically ORM class instances or objects of other types.
-# Types should be in the interfaces.py file. 
+# Types should be in the interfaces.py file.
